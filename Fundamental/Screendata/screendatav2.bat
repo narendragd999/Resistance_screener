05ced811d20bd12c8c793a39aae0f509bf2b9fcd
Narendra Dangi <narendradangi999@gmail.com>
2025-05-30 10:15:42 +0530

    conflict

---
+++ Fundamental/Screendata/screendatav2.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d D:\apps\Scanner\Resistance_screener\Fundamental\Screendata
+python -m streamlit run screendatav2.py
+pause
---
+++ lost-test-timer.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d D:\apps\Scanner\Resistance_screener
+python -m streamlit run lost-test1.py
+pause
